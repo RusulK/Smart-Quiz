d8a4b57728f0db42adda1d94240f08ecd4af5ed7
RusulK <rusulkamil14@gmail.com>
2021-10-24 23:08:31 +0000

    fitch-function

--- game.js
+++ game.js
@@ -10,80 +10,20 @@ let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
 
-let questions =[
-    {
-        question: "In which country is the Taj Mahal?",
-        choice1:"India",
-        choice2:"United Kingdom",
-        choice3:"Morocco",
-        answer: 1
-    },
-    {
-        question: "How many sides does a hexagon have?",
-        choice1:"2",
-        choice2:"5",
-        choice3:"6",
-        answer: 3
-    },
-    {
-        question: "What is the highest mountain in Great Britian?",
-        choice1:"Snowdon",
-        choice2:"Ben Nevis",
-        choice3:"Sca Fell",
-        answer: 2
-    },
-    {
-        question: "What is the name of the organ that pumps blood around the body?",
-        choice1:"Heart",
-        choice2:"Brain",
-        choice3:"Liver",
-        answer: 1
-    },
-    {
-        question: "How many hours are there in two days?",
-        choice1:"24h",
-        choice2:"48h",
-        choice3:"72h",
-        answer: 2
-    },
-    {
-        question: "What kind of food is pawpaw?",
-        choice1:"Fruit",
-        choice2:"Meat",
-        choice3:"Vegetable",
-        answer: 1
-    },
-    {
-        question: "What colour are sapphires?",
-        choice1:"Red",
-        choice2:"White",
-        choice3:"Blue",
-        answer: 3
-    },
-    {
-        question: "What are the two longest rivers in the world?",
-        choice1:"The Nile and The Thames",
-        choice2:"Amazon and Niger",
-        choice3:"The Nile and Amazon",
-        answer: 3
-    },
-    {
-        question: "How many days are there in June?",
-        choice1:"31 days",
-        choice2:"30 days",
-        choice3:"28 days",
-        answer: 2
-    },
-    {
-        question: "What is the female swan called?",
-        choice1:"Lilli",
-        choice2:"Julia",
-        choice3:"Pen",
-        answer: 3
-    },
+let questions = [];
+//loaded all Question
+fetch('question.json').then( res => {
+    return res.json();
+})
+.then(loadedQuestions => {
+    console.log(loadedQuestions);
+    questions = loadedQuestions;
+    startGame();
+})
 
+.catch( err =>{
 
-];
+});
 
 //Pint per score and number of questions per game
 const CORRECT_BONUS = 10;
@@ -161,5 +101,5 @@ incrementScore = num => {
 
 }
 
-startGame();
+
 
