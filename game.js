8e3cd0a3b700c63c27bce516c8cef0efefdf0997
RusulK <rusulkamil14@gmail.com>
2021-10-24 10:59:41 +0000

    final scores

--- game.js
+++ game.js
@@ -99,6 +99,9 @@ startGame = () => {
 
 getNewQuestion = () => {
     if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score);
+
+
         // Take the user to the end page
         return window.location.assign('./end.html');
     }
