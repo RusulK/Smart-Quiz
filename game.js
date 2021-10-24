0226ba33560abf8e44471cf0b4c4309bb9379474
RusulK <rusulkamil14@gmail.com>
2021-10-24 00:19:09 +0000

    progressBar

--- game.js
+++ game.js
@@ -1,7 +1,9 @@
+/* Constants and Variable*/
 const question = document.getElementById('question');
 const choices = Array.from(document.getElementsByClassName('choice-text'));
-const questionCounterText = document.getElementById('questionCounter');
+const progressText = document.getElementById('progressText');
 const scoreText = document.getElementById('score');
+const progressBarFull = document.getElementById('progressBarFull')
 let currentQuestion = {};
 let acceptingAnswers = false;
 let score = 0;
@@ -83,7 +85,7 @@ let questions =[
 
 ];
 
-//constants
+//Pint per score and number of questions per game
 const CORRECT_BONUS = 10;
 const MAX_QUESTIONS = 4;
 
@@ -91,49 +93,59 @@ startGame = () => {
     questionCounter = 0;
     score = 0;
     availableQuestions =[...questions];
-    console.log(availableQuestions);
     getNewQuestion();
 
 };
 
 getNewQuestion = () => {
     if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+        // Take the user to the end page
         return window.location.assign('./end.html');
     }
+//Update the Progress Bar
 questionCounter++;
-questionCounterText.innerText = questionCounter + "/" + MAX_QUESTIONS;
+progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
+progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
+
+
+
+
+// Update Questions
 const questionIndex = Math.floor(Math.random() * availableQuestions.length);
 currentQuestion = availableQuestions[questionIndex];
 question.innerText = currentQuestion.question;
 
+// Setup Choices
 choices.forEach ( choice => {
         const number = choice.dataset['number'];
         choice.innerText = currentQuestion['choice' + number];
-})
+});
+
 availableQuestions.splice(questionIndex, 1);
 
 acceptingAnswers = true;
 
 };
+
 choices.forEach(choice => {
     choice.addEventListener('click', e => {
         if(!acceptingAnswers) return;
         acceptingAnswers = false;
         const selectedChoice = e.target;
         const selectedAnswer = selectedChoice.dataset['number'];
+        
 
-        let classToApply = 'incorrect';
-        if (selectedAnswer == currentQuestion.answer) {
-            classToApply = 'correct' ;
-        };
+        // Applying css styling for correct and incorrect Answers
+        const classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
+
         if (classToApply == 'correct') {
             incrementScore(CORRECT_BONUS);
         };
-        selectedChoice.parentElement.classlist.add(classToApply);
+        selectedChoice.parentElement.classList.add(classToApply);
         
 
         setTimeout(() => {
-            selectedChoice.parentElement.classlist.remove(classToApply);
+            selectedChoice.parentElement.classList.remove(classToApply);
             getNewQuestion();
 
         }, 1000);
